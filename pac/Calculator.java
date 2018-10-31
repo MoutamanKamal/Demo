cb5da8658affefe2f1b70117b23a5d2ae1d928d4
Moutaman Kamal <mkgodot96@gmail.com>
2018-10-31 11:43:52 +0200

    A commit for the demo.

---
+++ pac/Calculator.java
@@ -0,0 +1,9 @@
+package pac;
+public classs Calculator {
+  public int evaluate(String expression) {
+    int sum = 0;
+    for (String summand: expression.split("\\+"))
+      sum += Integer.valueOf(summand);
+    return sum;
+  }
+}
