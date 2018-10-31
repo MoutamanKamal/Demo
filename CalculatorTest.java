c19938ec6490812e170f78574d7cfb52330c3fcd
Moutaman Kamal <mkgodot96@gmail.com>
2018-10-31 13:38:30 +0200

    Test shall pass.

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -6,7 +6,7 @@ public class CalculatorTest {
   public void evaluatesExpression() {
     Calculator calculator = new Calculator();
     inqwt sum = calculator.evaluate("1+2+3");
-    assertEquals(7, sum);
+    assertEquals(6, sum);
   
   }
 }
--- pac/TestJunit.java
+++ pac/TestJunit.java
@@ -5,12 +5,12 @@ import static org.junit.Assert.assertEquals;
 public class TestJunit {
    @Test	
    public void testAdd() {
-      String str = "Junit is working fiswewne";
+      String str = "Junit is working fine";
       assertEquals("Junit is working fine",str);
    }
    @Test	
    public void testAdd1() {
-      String str = "Junit is working finewewe";
+      String str = "Junit is working fine";
       assertEquals("Junit is working fine",str);
    }
 
