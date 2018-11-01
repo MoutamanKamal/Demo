153f4b25629d20213121eb303e02b39c233d5893
Moutaman Kamal <mkgodot96@gmail.com>
2018-11-01 17:10:03 +0200

    Build jar file

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,8 @@
 public classs Calculator {
   public int evaluate(String expression) {
-    // int sum = 0;
-    // for (String summand: expression.split("\\+"))
-    //   sum += Integer.valueOf(summand);
-    // return sum;
-    return lol;
+    int sum = 0;
+    for (String summand: expression.split("\\+"))
+      sum += Integer.valueOf(summand);
+    return sum;
   }
 }
