99ec301a0a9c64cf4952455609f9254486c4453b
Moutaman Kamal <mkgodot96@gmail.com>
2018-11-02 09:42:24 +0200

    Trick

--- pac/TestJunit.java
+++ pac/TestJunit.java
@@ -5,12 +5,12 @@ import static org.junit.Assert.assertEquals;
 public class TestJunit {
    @Test	
    public void testAdd() {
-      String str = "Junit is working fine";
+      String str = "Junit is working fiene";
       assertEquals("Junit is working fine",str);
    }
    @Test	
    public void testAdd1() {
-      String str = "Junit is working fine";
+      String str = "Junit is working fi4ne";
       assertEquals("Junit is working fine",str);
    }
 
