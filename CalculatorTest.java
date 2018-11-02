2ec84a621cb66a23f226111701bf0631a537ed07
Moutaman Kamal <mkgodot96@gmail.com>
2018-11-02 09:43:50 +0200

    Tests will fail.

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -6,7 +6,7 @@ public class CalculatorTest {
   public void evaluatesExpression() {
     Calculator calculator = new Calculator();
     int sum = calculator.evaluate("1+2+3");
-    assertEquals(6, sum);
+    assertEquals(7, sum); // 6 != 7 test will fail
   
   }
 }
