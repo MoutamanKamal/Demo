88b640b75c5c9dbb71c3b3e79550ff7429ee8a9e
Moutaman Kamal <mkgodot96@gmail.com>
2018-11-13 16:05:08 +0200

    Tests will faill.

--- pac/TestJunit.java
+++ pac/TestJunit.java
@@ -1,16 +1,16 @@
-ppackage pac;
+package pac;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestJunit {
    @Test	
    public void testAdd() {
-      String str = "Junit is working fine";
+      String str = "Junit is wordsking fine";
       assertEquals("Junit is working fine",str);
    }
    @Test	
    public void testAdd1() {
-      String str = "Junit is working fine";
+      String str = "Junit is workisdsng fine";
       assertEquals("Junit is working fine",str);
    }
 
